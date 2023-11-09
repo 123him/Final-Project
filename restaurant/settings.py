1a0bd7d88f9e2b674d40a4d4078b57031959931b
hima <hima95mohan@gmail.com>
2023-11-09 11:38:18 +0530

    up

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x381ku9tdvkk%wc=c8$lzcext4g%4ywv7f%v_qigufv6rl2@lq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
